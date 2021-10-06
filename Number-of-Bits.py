ed53fec1198a904319801e243af011f5b9b946ac
Mohammad Ahmad ALhamad <maalslab@gmail.com>
2021-10-06 07:30:31 +0300

    Creating Project Files

---
+++ Number-of-Bits.py
@@ -0,0 +1,22 @@
+#############################################################################
+########        This Program Calculates Number of Bits needed        ########
+########                Needed for a decimal Number                  ########
+########                                                             ########
+########            N = log2(decimal number)                         ########
+########            N = Number of bits                               ########
+#############################################################################
+import math
+
+D_number = input('Decimal number value = ')
+
+if D_number.isnumeric():
+    D_number = int(D_number) # Convert to int
+elif '.' in D_number:
+    D_number = float(D_number)
+else:
+    print("Pleas Enter a Decimal Number")
+    exit()
+
+number_of_bits = round(math.log2(D_number))
+
+print(f'Number of Bits = {number_of_bits}')
